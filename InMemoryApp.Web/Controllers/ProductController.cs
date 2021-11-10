864ce281fc6d3e682062d78b3c6b6a0401346197
muratilgun <muratilgun34@gmail.com>
2021-11-11 00:17:23 +0300

    IMemoryCache Get() ve set() methods

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -18,6 +18,13 @@ namespace InMemoryApp.Web.Controllers
 
         public IActionResult Index()
         {
+            _memoryCache.Set<string>("zaman",DateTime.Now.ToString());
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
             return View();
         }
     }
