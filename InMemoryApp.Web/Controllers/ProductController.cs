2424353fe4045857e0aeff0962bdfb88f37c12e8
muratilgun <muratilgun34@gmail.com>
2021-11-11 15:47:24 +0300

    Cache Priority

--- InMemoryApp.Web/Controllers/ProductController.cs
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -41,6 +41,7 @@ namespace InMemoryApp.Web.Controllers
             //beraber kullanılması best pratice
             options.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
             options.SlidingExpiration = TimeSpan.FromSeconds(10);
+            options.Priority = CacheItemPriority.High;
             _memoryCache.Set<string>("zaman", DateTime.Now.ToString(), options);
 
             return View();
