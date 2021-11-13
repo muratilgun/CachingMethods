19021a2422930404a837bc9ae8ef6adde5053de6
muratilgun <muratilgun34@gmail.com>
2021-11-13 21:48:29 +0300

    added SortedSet Type Controller

---
+++ RedisExchangeAPI.Web/Controllers/SortedSetTypeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Service;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class SortedSetTypeController : Controller
+    {
+
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;
+        private string listKey = "hashnames";
+        public SortedSetTypeController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(2);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
