86b88c21027376a26f1e4a4db37497cb97adb133
muratilgun <muratilgun34@gmail.com>
2021-11-13 14:30:08 +0300

    IDistributedCache basics

--- IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -16,6 +16,22 @@ namespace IDistributedCacheRedisApp.Web.Controllers
         }
         public IActionResult Index()
         {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+            _distributedCache.SetString("name", "Murat", cacheEntryOptions); 
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("name");
+            ViewBag.name = name;
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
             return View();
         }
     }
